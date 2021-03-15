1f19f52b5ec2316bb311ed0ebffaff9078fc479d
lipan1998 <2867684546@qq.com>
2021-03-15 08:56:40 +0800

    constantly updating

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -1,66 +1,46 @@
 // pages/cart/cart.js
+/* 
+1. 获取用户的收货地址
+   1 绑定点击事件
+   2 获取小程序内置 API 获取用户的收货地址 wx.chooseAddress
+2. 获取 用户 对小程序 所授予 获取地址的权限 状态 scope  authSetting  scope.address
+  1 假设 用户 点击获取收货地址的提示框为 确定 
+    scope 值 true 直接调用获取收货地址
+  2 假设 用户 点击获取收货地址的提示框为 取消
+    scope 值 false
+    1 诱导用户 自己打开 授权页面 当用户重新给与 获取地址权限的时候
+    2 获取收货地址
+  3 假设用户 从来没有调用过 收货地址的API
+    scope undefined 直接调用获取收货地址
+注意：新公告 三个接口全部为true
+scope.address: true
+scope.invoice: true
+scope.invoiceTitle: true
+  4 将收货地址存入到缓存中
+
+
+*/
+import { getSetting, chooseAddress, openSetting } from "../../utils/asyncWx.js";
+import { regeneratorRuntime } from "../../lib/runtime/runtime.js"
 Page({
+  // 1. 点击收货地址
+  async handleChooseAddress() {
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    // 2. 获取权限状态
+    try {
+      const result1 = await getSetting();
+      // 1 获取权限状态 只要发现一些变量名很怪异的时候 都要用[]括起来
+      const scopAddress = result1.authSetting['scope.address'];
+      if (scopAddress === false) {
+        // 2 用户以前拒绝过授予权限 先诱导用户打开授予权限
+        await openSetting();
+      }
+      // 3 重新调用收货地址的API
+      const address = await chooseAddress();
+      wx.setStorageSync("address", address);
 
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    } catch (error) {
+      console.log(error);
+    }
   }
 })
--- pages/goods_detail/goods_detail.js
+++ pages/goods_detail/goods_detail.js
@@ -11,7 +11,7 @@ Page({
     goodsObj: {}
 
   },
-
+  GoodsObj: {},
   /**
    * 生命周期函数--监听页面加载
    */
@@ -23,9 +23,59 @@ Page({
   // 1. 获取商品详情数据
   async getGoodsDetail(goods_id) {
     const goodsObj = await request({ url: '/goods/detail', data: { goods_id } });
+    this.GoodsObj = goodsObj;
     this.setData({
-      goodsObj
+      goodsObj: {
+        goods_name: goodsObj.goods_name,
+        // ipone 部分手机 不识别webp图片格式
+        // 最好找到后台 让他修改
+        // 临时修改 确保后台存在 1.webp => 1.jpg
+        goods_introduce: goodsObj.goods_introduce.replace(/\.webp/g, '.jpg'),
+        goods_price: goodsObj.goods_price,
+        pics: goodsObj.pics
+
+
+      }
     })
+  },
+  // 点击轮播图放大预览
+  handlePrevewImage(e) {
+    // console.log("放大");
+    // 1 先构造要预览的图片数组
+    const urls = this.GoodsObj.pics.map((v => v.pics_mid));
+    // 2 接收传递过来的图片 url
+    const current = e.currentTarget.dataset.url;
+    wx.previewImage({
+      current,
+      urls
+
+    });
+  },
+  // 点击加入购物车
+  handleCartAdd() {
+    console.log("加入");
+    // 1 获取缓存中的购物车数据 数组
+    let cart = wx.getStorageSync("cart") || [];
+    // 2 判断当前商品是否已存在于购物车
+    let index = cart.findIndex(v => v.goods_id === this.GoodsObj.goods_id);
+    if (index === -1) {
+      // 3 不存在 第一次添加
+      this.GoodsObj.num = 1;
+      cart.push(this.GoodsObj)
+    } else {
+      // 4 已将存在购物车 执行num++
+      cart[index].num++;
+    }
+    // 5 把数组重新填充回缓存中
+    wx.setStorageSync("cart", cart);
+    // 6 弹窗提醒
+    wx.showToast({
+      title: '加入成功',
+      icon: 'success',
+      // 防止用户手抖 疯狂点击按钮
+      mask: true,
+
+    });
   }
 
 
